c2485c943fdf054be0d1f2273613bd45c7d9c585
Jeff Cox <jeffcox@me.com>
2017-11-26 15:57:40 -0800

    Changes to k init script.  I want -h, but not everything has the right deps.
    TODO: Convert all my [[ -x $(which) ]] calls into type?

--- modules/k/init.zsh
+++ modules/k/init.zsh
@@ -1,5 +1,14 @@
 # Quick, dirty, and at least a little Prezto flavored.
 
 source ${ZDOTDIR:-$HOME}/.zprezto/modules/k/external/k.sh
+
+# Check for g/numfmt and enable -h by default since I am a human
+if [[ -x $(which gnumfmt 2>/dev/null) ]]; then
+  alias k='k -h'
+else
+  if [[ -x $(which numfmt 2>/dev/null) ]]; then
+    alias k='k -h'
+  fi
+fi
+
 alias ka='k -a'
-alias kh='k -h'
