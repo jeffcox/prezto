2c1ccb80fb38249ea706e59e7ca1c2f48b3d1b42
Jeff Cox <jcox@paloaltonetworks.com>
2019-01-10 12:17:55 -0800

    Finished detectos module.  First run was slow, more testing needed.

--- modules/detectos/init.zsh
+++ modules/detectos/init.zsh
@@ -1,15 +1,18 @@
 # Detect the host OS and load appropriate zprezto modules
 # This should be used in lieu of the specific modules in zpreztorc
 
-
-# Modules are either loaded or have their state storied in zstyle.  zprezto init.zsh holds the secrets
-
+# Conditional soup to emulate init.zsh for each OS
+# This loads the necessary modules
 if [[ "$OSTYPE" == freebsd* ]]; then
-  return 1
+  source ${ZPREZTODIR}/modules/freebsd/init.zsh && zstyle ":prezto:module:freebsd" loaded 'yes'
 elif [[ "$OSTYPE" == darwin* ]]; then
-  return 1
+  source ${ZPREZTODIR}/modules/osx/init.zsh && zstyle ":prezto:module:osx" loaded 'yes'
+  source ${ZPREZTODIR}/modules/macports/init.zsh && zstyle ":prezto:module:macports" loaded 'yes'
 elif [[ "$OSTYPE" == linux-gnu ]]; then
-  return 1
+  #ugh, linux, let's shotgun this bitch
+  source ${ZPREZTODIR}/modules/dnf/init.zsh && zstyle ":prezto:module:dnf" loaded 'yes'
+  source ${ZPREZTODIR}/modules/dpkg/init.zsh && zstyle ":prezto:module:dpkg" loaded 'yes'
+  source ${ZPREZTODIR}/modules/yum/init.zsh && zstyle ":prezto:module:yum" loaded 'yes'
 else
   echo "You've invoked the detectos module but it could not detect your OS"
   echo "Please configure zpreztorc manually.  Sorry."
